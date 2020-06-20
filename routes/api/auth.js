e35be2c4e4bd9e8a0a81fc9219914e219e1defab
ajay palsaniya <ajaykumarpalsaniya@gmail.com>
2020-06-20 23:59:30 +0530

    add auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route  Post api/auth
 // @desc   Register user
 // @access Public
 
-router.get("/", (req, res) => {
-  res.send("auth route");
+router.get("/", auth, (req, res) => {
+  res.send("Auth route");
 });
 
 module.exports = router;
