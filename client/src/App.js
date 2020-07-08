4405d2673f0e5fc5109e9f7ded3d681aa6b28f09
ajay kumar palsaniya <ajaykumarpalsaniya@gmaile.com>
2020-07-08 09:40:42 +0530

    Ready to deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import './App.css';
+import Routes from './components/routing/Routes';
 
-//Redux
+// Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<Navbar />
-				<Route exact path='/' component={Landing} />
-				<section className='container'>
-					<Alert />
-					<Switch>
-						<Route exact path='/register' component={Register} />
-						<Route exact path='/login' component={Login} />
-					</Switch>
-				</section>
-			</Fragment>
-		</Router>
-	</Provider>
-);
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,56 +1,72 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-function Login() {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	});
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
 
-	const { email, password } = formData;
+  const { email, password } = formData;
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		console.log(formData);
-	};
-	return (
-		<Fragment>
-			<h1 className='large text-primary'>Sign IN</h1>
-			<p className='lead'>
-				<i className='fas fa-user'></i> Sign Into Your Account
-			</p>
-			<form className='form' onSubmit={(e) => onSubmit(e)}>
-				<div className='form-group'>
-					<input
-						type='email'
-						placeholder='Email Address'
-						value={email}
-						name='email'
-						onChange={(e) => onChange(e)}
-						required
-					/>
-				</div>
-				<div className='form-group'>
-					<input
-						type='password'
-						placeholder='Password'
-						name='password'
-						onChange={(e) => onChange(e)}
-						value={password}
-						minLength='6'
-					/>
-				</div>
+  const onSubmit = e => {
+    e.preventDefault();
+    login(email, password);
+  };
 
-				<input type='submit' className='btn btn-primary' value='Register' />
-			</form>
-			<p className='my-1'>
-				Don't have an account? <Link to='/register'>Sign Up</Link>
-			</p>
-		</Fragment>
-	);
-}
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
-export default Login;
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
