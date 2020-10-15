3f1546089018f21476da05341860e3cedc786777
Ajay Palsaniya <ajaykumarpalsaniya@gmail.com>
2020-10-15 17:26:12 +0530

    updated

---
+++ client/build/precache-manifest.17197e8342fd14ab1c8e240d458de205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc891cdb573943ab328a0aa2902f403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac18aa4275cddf90fc4f",
+    "url": "/static/css/main.3645c1ea.chunk.css"
+  },
+  {
+    "revision": "042b3d79aa0fc2eb47ee",
+    "url": "/static/js/2.c03919f7.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.c03919f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac18aa4275cddf90fc4f",
+    "url": "/static/js/main.4ac24e09.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dca5c5333f6686d416d3bbadbb2fd3bb",
+    "url": "/static/media/showcase.dca5c533.jpg"
+  }
+]);
